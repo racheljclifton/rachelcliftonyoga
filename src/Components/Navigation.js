f891c08ee1e25a2df97fe4189876dfad2905c54b
Rachel Clifton <rachel@Rachels-Air.lan>
2021-05-17 11:36:04 -0400

    removed navigation styles import

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import styles from './Navigation.module.css';
+// import styles from './Navigation.module.css';
 import Button from './UI/Button';
 
 const Navigation = (props) => {

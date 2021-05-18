0cb4e08a1ba89fb8137200f5f6811ee48f9d141d
Rachel Clifton <racheljclifton@gmail.com>
2021-05-18 16:50:42 -0400

    caption regex fix

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -23,8 +23,7 @@ const Gallery = () => {
   listOfImages = initialListOfImages.reverse();
 
   const caption = (fileName) => {
-    // eslint-disable-next-line
-    return fileName.match(/(?<=\/)(?!.*\/)[^\/]*?(?=\.)/)[0];
+    return fileName.match(/(?<=\/)(?!.*\/)[^/]*?(?=\.)/)[0];
   };
 
   const showPictureHandler = (event) => {

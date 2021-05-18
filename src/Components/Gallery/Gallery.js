3c8782d7ac68cc1170d05679cac38abad585f407
Rachel Clifton <racheljclifton@gmail.com>
2021-05-18 16:38:56 -0400

    added captions to images in gallery

--- src/Components/Gallery/Gallery.js
+++ src/Components/Gallery/Gallery.js
@@ -9,7 +9,8 @@ let listOfImages = [];
 
 const Gallery = () => {
   const [isShowingPicture, setIsShowingPicture] = useState(false);
-  const [modalImageSrc, setModalImageSrc] = useState("");
+  // const [modalImageSrc, setModalImageSrc] = useState(undefined);
+  const [modalMessage, setModalMessage] = useState('');
 
   const importAll = (r) => {
     return r.keys().map(r);
@@ -21,19 +22,47 @@ const Gallery = () => {
 
   listOfImages = initialListOfImages.reverse();
 
+  const caption = (fileName) => {
+    return fileName.match(/(?<=\/)(?!.*\/)[^\/]*?(?=\.)/)[0];
+  };
+
   const showPictureHandler = (event) => {
     setIsShowingPicture(true);
-    setModalImageSrc(event.target.currentSrc);
+    // setModalImageSrc(event.target.currentSrc);
+    
+    const initialText = caption(event.target.currentSrc);
+    const splitText = initialText.split('%20');
+    splitText.shift();
+    const text = splitText.join(' ');
+
+    setModalMessage(      <Fragment>
+      <img src={event.target.currentSrc} alt={text} className={styles.selected} />
+      <p>{text}</p>{" "}
+    </Fragment>)
   };
 
   const hidePictureHandler = (event) => {
     setIsShowingPicture(false);
-    setModalImageSrc("");
+    // setModalImageSrc("");
+    setModalMessage('');
   };
 
-  const caption = (fileName) => {
-    return fileName.match(/(?<=\/)(?!.*\/)[^\/]*?(?=\.)/)[0];
-  };
+  // const setMessage = (url, text) => {
+  //   return (
+  //     <Fragment>
+  //       <img src={url} alt={text} className={styles.selected} />
+  //       <p>{text}</p>{" "}
+  //     </Fragment>
+  //   );
+  // };
+
+  // let message; 
+  // useEffect(() => {
+  //   if (modalImageSrc) {
+  //     const text = caption(modalImageSrc);
+  //     message = setMessage(modalImageSrc, text);
+  //   }
+  // }, [modalImageSrc]);
 
   const img = (image, index) => {
     return (
@@ -48,8 +77,6 @@ const Gallery = () => {
     );
   };
 
-  // const message =
-
   return (
     <>
       <header>
@@ -59,16 +86,7 @@ const Gallery = () => {
         {listOfImages.map((image, index) => img(image, index))}
       </div>
       {isShowingPicture === true && (
-        <Modal
-          onConfirm={hidePictureHandler}
-          message={
-            <img
-              src={modalImageSrc}
-              alt={caption(modalImageSrc)}
-              className={styles.selected}
-            />
-          }
-        />
+        <Modal onConfirm={hidePictureHandler} message={modalMessage} />
       )}
     </>
   );
